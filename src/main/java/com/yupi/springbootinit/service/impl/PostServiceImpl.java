f94c5f4984dca7047368d39932ff10fd13ea0408
1-鱼皮 <592789970@qq.com>
2023-12-27 15:36:19 +0000

    fix 修复帖子查询 Bug

--- src/main/java/com/yupi/springbootinit/service/impl/PostServiceImpl.java
+++ src/main/java/com/yupi/springbootinit/service/impl/PostServiceImpl.java
@@ -112,7 +112,7 @@ public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements Po
         Long notId = postQueryRequest.getNotId();
         // 拼接查询条件
         if (StringUtils.isNotBlank(searchText)) {
-            queryWrapper.like("title", searchText).or().like("content", searchText);
+            queryWrapper.and(qw -> qw.like("title", searchText).or().like("content", searchText));
         }
         queryWrapper.like(StringUtils.isNotBlank(title), "title", title);
         queryWrapper.like(StringUtils.isNotBlank(content), "content", content);
